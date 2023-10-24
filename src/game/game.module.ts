89648a2528d826470a1f63f18e3779c0635fd29d
suhwan <tnghkswkd123@naver.com>
2023-10-25 05:54:43 +0900

    [feat] win_loss_record entity.ts service.ts 구현
    1. game/database/ 에 record를 추가함
    2. database에 둘 이상이 있을때 처리가 제대로 된 상태는 아님

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -4,10 +4,11 @@ import { GameService } from './game.service';
 import { HistoryService } from './database/history/history.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { History } from './database/history/history.entity';
+import { RecordService } from './database/record/record.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([History])],
   controllers: [GameController],
-  providers: [GameService, HistoryService]
+  providers: [GameService, HistoryService, RecordService]
 })
 export class GameModule {}
