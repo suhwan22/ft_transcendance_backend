21acfabdfece73cfa210d987703db1ce75ba649d
wkdtpgns5016 <wkdtpgns5016@naver.com>
2023-11-21 15:46:03 +0900

    [feat] REGIST 게임 프로토콜 구현
    1. REGIST 게임 프로토콜 구현

--- src/games/games.gateway.ts
+++ src/games/games.gateway.ts
@@ -10,12 +10,18 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ChatsGateway } from 'src/chats/chats.gateway';
+import { LobbyGateway } from 'src/sockets/lobby/lobby.gateway';
+import { UsersService } from 'src/users/users.service';
 
 @WebSocketGateway(3131, { namespace: '/games' })
 export class GamesGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     @Inject(forwardRef(() => ChatsGateway))
-    private readonly chatsGateway: ChatsGateway) {
+    private readonly chatsGateway: ChatsGateway,
+    @Inject(forwardRef(() => LobbyGateway))
+    private readonly lobbyGateway: LobbyGateway,
+    private readonly usersService: UsersService,
+    ) {
     this.clients = new Map<number, Socket>();
   }
 
@@ -29,6 +35,28 @@ export class GamesGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   //소켓 연결 해제시 유저목록에서 제거
   async handleDisconnect(client: Socket): Promise<void> {
-    console.log('game disconnected', client.id);
+    const key = client.data.userId;
+    if (!key)
+      return ;
+    this.clients.delete(key);
+    this.usersService.updatePlayerStatus(key, 3);
+    this.chatsGateway.sendUpdateToChannelMember(key);
+    this.lobbyGateway.sendUpdateToFriends(key);
+    console.log('chat disonnected', client.id);
+  }
+
+
+  @SubscribeMessage('REGIST')
+  async registUserSocket(client: Socket, userId: number) {
+    this.clients.set(userId, client);
+    client.data.userId = userId;
+    this.usersService.updatePlayerStatus(userId, 1);
+    this.chatsGateway.sendUpdateToChannelMember(userId);
+    this.lobbyGateway.sendUpdateToFriends(userId);
+  }
+
+  @SubscribeMessage('MATCH')
+  async matchMaking(client: Socket, userId: number) {
+    console.log('match making');
   }
 }
