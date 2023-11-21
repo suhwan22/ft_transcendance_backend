b2d4362c929fe0492dbc30a69410fb5cef40366a
wkdtpgns5016 <wkdtpgns5016@naver.com>
2023-11-21 16:12:06 +0900

    [docs] game 임시 html 파일 생성

--- src/games/games.gateway.ts
+++ src/games/games.gateway.ts
@@ -20,14 +20,16 @@ export class GamesGateway implements OnGatewayConnection, OnGatewayDisconnect {
     private readonly chatsGateway: ChatsGateway,
     @Inject(forwardRef(() => LobbyGateway))
     private readonly lobbyGateway: LobbyGateway,
-    private readonly usersService: UsersService,
-    ) {
+    private readonly usersService: UsersService,) {
     this.clients = new Map<number, Socket>();
+    this.queue = [];
   }
 
   @WebSocketServer()
   server: Server;
   clients: Map<number, Socket>;
+  queue: Socket[];
+
   //소켓 연결시 유저목록에 추가
   public handleConnection(client: Socket, ...args: any[]): void {
     console.log('game connected', client.id);
@@ -48,9 +50,10 @@ export class GamesGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage('REGIST')
   async registUserSocket(client: Socket, userId: number) {
-    this.clients.set(userId, client);
     client.data.userId = userId;
-    this.usersService.updatePlayerStatus(userId, 1);
+    client.data.math = false;
+    this.clients.set(userId, client);
+    this.usersService.updatePlayerStatus(userId, 2);
     this.chatsGateway.sendUpdateToChannelMember(userId);
     this.lobbyGateway.sendUpdateToFriends(userId);
   }
@@ -58,5 +61,19 @@ export class GamesGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage('MATCH')
   async matchMaking(client: Socket, userId: number) {
     console.log('match making');
+    // 대기 큐에 넣기
+    this.queue.push(client);
+    // 대기 큐에 헤드에 오거나 매칭이 잡혔다면 탈출
+    while (this.queue[0].data.userId === userId || client.data.math === true)
+      client.emit("NOTICE", "waitting...");
+    if (client.data.math)
+      return ;
+    // 대기 큐에 사람이 올때 까지 대기
+    while (this.queue.length > 0)
+      client.emit("NOTICE", "waitting...");
+    const other = this.queue.shift();
+    other.data.math = true;
+    client.emit("NOTICE", "success");
+    other.emit("NOTICE", "sucesss");
   }
 }
--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -6,11 +6,13 @@ import { GameHistory } from './entities/game-history.entity';
 import { UsersModule } from 'src/users/users.module';
 import { ChatsModule } from 'src/chats/chats.module';
 import { GamesGateway } from './games.gateway';
+import { SocketsModule } from 'src/sockets/sockets.module';
 
 @Module({
   imports: [
     forwardRef(() => (UsersModule)),
     forwardRef(() => (ChatsModule)),
+    forwardRef(() => (SocketsModule)),
     TypeOrmModule.forFeature([
     GameHistory])],
   controllers: [GamesController],
