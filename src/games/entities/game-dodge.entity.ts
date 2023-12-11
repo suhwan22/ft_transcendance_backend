6e5df67ec9d43d2f954be5078135f3f085545e05
wkdtpgns5016 <wkdtpgns5016@naver.com>
2023-12-11 14:39:37 +0900

    [feat] 게임 기능 추가 요청 구현
    1. 닷지 패널티 기능 구현
    2. 최근 전적 조회 API 구현
    
    resolved #68

---
+++ src/games/entities/game-dodge.entity.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Player } from "src/users/entities/player.entity";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn, UpdateDateColumn, Unique, } from "typeorm";
+
+@Entity({ name: "game_dodge" })
+@Unique(['user'])
+export class GameDodge {
+
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty({ type: () => Player})
+  @ManyToOne( (type) => Player, (player) => player.gameHistory)
+  @JoinColumn({ name: 'user_id' })
+  user: Player;
+
+  @ApiProperty()
+  @UpdateDateColumn()
+  date: Date;
+}
