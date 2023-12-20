b2ef7f1fbdeebd0498034d9ef043e214502e96e1
wkdtpgns5016 <wkdtpgns5016@naver.com>
2023-12-20 12:14:38 +0900

    [fix] NOTICE filter 적용

--- src/sockets/sockets.exception.filter.ts
+++ src/sockets/sockets.exception.filter.ts
@@ -7,31 +7,32 @@ export class SocketExceptionFilter extends BaseWsExceptionFilter {
     const client = host.switchToWs().getClient();
     console.log(exception);
     if (exception.name === 'JsonWebTokenError') {
-      const msg = this.getNotice("Invaild Token", 201);
+      const msg = this.getNotice("Invaild Token", 201, client.data.status);
       client.emit("NOTICE", msg);
     }
     else if (exception.name === 'TokenExpiredError') {
-      const msg = this.getNotice("Token expired", 202);
+      const msg = this.getNotice("Token expired", 202, client.data.status);
       client.emit("NOTICE", msg);
     }
     else if (exception.error === 'DuplicatedAccessError') {
-      const msg = this.getNotice("Duplicated Access", 203);
+      const msg = this.getNotice("Duplicated Access", 203, client.data.status);
       client.emit("NOTICE", msg);
     }
     else if (exception.error === 'TokenExpiredError') {
-      const msg = this.getNotice("Token expired", 202);
+      const msg = this.getNotice("Token expired", 202, client.data.status);
       client.emit("NOTICE", msg);
     }
     else {
-      const msg = this.getNotice("DB Error", 200);
+      const msg = this.getNotice("DB Error", 200, client.data.status);
       client.emit("NOTICE", msg);
     }
   }
 
-  getNotice(message: string, code: number) {
+  getNotice(message: string, code: number, status: number) {
     return ({
       code: code,
       content: message,
+      status: status,
       date: new Date(),
     });
   }
